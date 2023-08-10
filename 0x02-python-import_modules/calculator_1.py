f5cd40ebe1d9ef060b910633f9af79320b6de57d
hudini46 <owenkiilu37@gmail.com>
2023-08-10 08:28:08 +0000

    import &modules

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,47 @@
+ddition(x, y):
+    """My addition function
+
+    Args:
+        x: first integer
+        y: second integer
+
+    Returns:
+        The return value. x + y
+    """
+    return (x + y)
+
+def subtraction(x, y):
+    """My subtraction function
+
+    Args:
+        x: first integer
+        y: second integer
+
+    Returns:
+        The return value. x - y
+    """
+    return (x - y)
+
+def multiplication(x, y):
+    """My multiplication function
+
+    Args:
+        x: first integer
+        y: second integer
+
+    Returns:
+        The return value. x * y
+    """
+    return (x * y)
+
+def division(x, y):
+    """My division function
+
+    Args:
+        x: first integer
+        y: second integer
+
+    Returns:
+        The return value. x / y
+    """
+    return int(x / y)
