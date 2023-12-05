20702dbff495b3e9b46c12b89a73436ec04b8487
hudini46 <owenkiilu37@gmail.com>
2023-12-05 00:58:08 -0800

    networlong python

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A script that will
+- take in a URL
+- send a request to the URL
+- display the body of the response.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
